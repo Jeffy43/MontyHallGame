329a618e21be88af649b8dbb68c26025654a184f
Jeffy <jeffyp@nycstudents.net>
2023-11-07 18:23:21 -0500

    Update

--- src/MontyHall.java
+++ src/MontyHall.java
@@ -1,48 +1,133 @@
-public class MontyHall {
+public class MontyHall{
     private String guess;
-    private String gate1;
-    private String gate2;
-    private String gate3;
-
-    public MontyHall(int guess) {
-        if (guess == 1) {
-            this.guess = gate1;
-        } else if (guess == 2) {
-            this.guess = gate2;
-        } else {
-            this.guess = gate3;
-        }
-    }
-
+    private String behindguess;
+    private String door1 = "";
+    private String door2 = "";
+    private String door3 = "";
+    boolean reveal1;
+    boolean reveal2;
+    boolean reveal3;
+    boolean swap;
+    //randomly assigns on door to car and the rest to goat
     public void generate() {
         int a = (int) (Math.random() * 3);
-        int b = a - 1;
-        int c = a + 1;
-        if (a > b && a > c) {
-            gate1 = "Car";
-        } else if (b > a && b > c) {
-            gate2 = "Car";
+        if (a == 0) {
+            door1 = "Car";
+            door2 = "Goat";
+            door3 = "Goat";
+        } else if (a == 1) {
+            door1 = "Goat";
+            door2 = "Car";
+            door3 = "Goat";
         } else {
-            gate3 = "Car";
-        }
-        if (gate1.isEmpty()) {
-            gate1 = "Goat";
-        }
-        if (gate2.isEmpty()) {
-            gate2 = "Goat";
-        }
-        if (gate3.isEmpty()) {
-            gate3 = "Goat";
+            door1 = "Goat";
+            door2 = "Goat";
+            door3 = "Car";
         }
     }
-    public String otherGoat(){
-        int a = (int)(Math.random()*2);
-        String other;
-        if(guess.equals(gate1)) {
+    //assigns guess to what is behind the door they chose
+    public MontyHall(int guess){
+        if (guess == 1) {
+            this.guess = "door 1";
+            this.behindguess = door1;
+            reveal1 = true;
+        } else if (guess == 2) {
+            this.guess = "door 2";
+            this.behindguess = door2;
+            reveal2 = true;
+
+        } else {
+            this.guess = "door 3";
+            this.behindguess = door3;
+            reveal3 = true;
+
+        }
+    }
+
+    public MontyHall(String swap){
+        if(swap.equals("Yes")){
+            this.swap = true;
+        }else{
+            this.swap = false;
+        }
+    }
+
+    public String otherGoat() {
+        int a = (int) (Math.random() * 2);
+        String statement = "";
+        String other = "";
+        String other2 = "";
+
+        if (door1.equals("Goat")) {
+            other = "door 1";
+        } else if (door2.equals("Goat")) {
+            other = "door 2";
+        }
+
+        if (door2.equals("Goat")) {
+            other2 = "door 2";
+        } else if (door3.equals("Goat")) {
+            other2 = "door 3";
+        }
+
+        if (guess.equals("Car")) {
             if (a > 0) {
-                other = "Gate 1";
+                statement = "The other goat is in " + other;
+                if(other.equals("door 1")){
+                    reveal1 = true;
+                }else{
+                    reveal2 = true;
+                }
             } else {
-                other = "gate 2";
+                statement = "The other goat is in " + other2;
+                if(other2.equals("door 2")){
+                    reveal2 = true;
+                }else{
+                    reveal3 = true;
+                }
+            }
+        } else {
+            if (guess.equals("door1") && door2.equals("Goat")) {
+                statement = "The other goat is in door 2.";
+            } else if (guess.equals("door1") && door3.equals("Goat")) {
+                statement = "The other goat is in door 3.";
+            } else if (guess.equals("door2") && door3.equals("Goat")) {
+                statement = "The other goat is in door 3.";
+            }else if (guess.equals("door2") && door1.equals("Goat")) {
+                statement = "The other goat is in door 1.";
+            }else if (guess.equals("door3") && door1.equals("Goat")) {
+                statement = "The other goat is in door 1.";
+            }else{
+                statement = "The other goat is in door 2.";
             }
         }
+        return statement;
+    }
+
+    public void swap(){
+        if(swap){
+            if(guess.equals("door 1")){
+                if(reveal2){
+                    this.guess = "door 3";
+                }else{
+                    this.guess = "door 2";
+                }
+            }
+            if(guess.equals("door 2")){
+                if(reveal3){
+                    this.guess = "door 1";
+                }else{
+                    this.guess = "door 3";
+                }
+            }
+            if(guess.equals("door 3")){
+                if(reveal1){
+                    this.guess = "door 2";
+                }else{
+                    this.guess = "door 1";
+                }
+            }
+        }
+        System.out.println(this.guess);
+    }
 }
--- src/MontyHallRunner.java
+++ src/MontyHallRunner.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-public class MontyHallRunner {
+public class MontyHallRunner{
     public static void main(String[]args){
-        boolean swap;
+
+        //introduces game
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Monty Hall Game!\n");
         System.out.println("There are two goats and a car randomly behind door 1,2 and 3.");
         System.out.println("Where do you think the car is? Enter 1, 2, or 3: ");
         int guess = scan.nextInt();
-        new MontyHall(guess);
-
+        MontyHall game = new MontyHall(guess);
+        game.generate();
+        System.out.println(game.otherGoat());
         System.out.println("Do you want to switch your guess? Yes or No: ");
-        String a = scan.nextLine();
-        if (a.equals("Yes") || a.equals("yes")){
-            swap = true;
-        }else{
-            swap = false;
+        String swap = scan.nextLine();
+        MontyHall game2 = new MontyHall(swap);
+        game2.swap();
         }
     }
-}
